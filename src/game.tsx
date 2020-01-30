251757446723eac4c15d4e1c8c8eb19241e20486
Romain Fromi <romain.fromi@gmail.com>
2020-01-30 17:44:43 +0100

    Display draft cards using Hand component and add background image to the game

--- src/game.tsx
+++ src/game.tsx
@@ -1,12 +1,13 @@
 import {css} from '@emotion/core'
 import React, {Fragment} from 'react'
-import {Game, useGame} from 'tabletop-game-workshop'
+import {Game, Hand, useGame} from 'tabletop-game-workshop'
 import Header from './Header'
 import ItsAWonderfulWorld from './ItsAWonderfulWorld'
 import DevelopmentCard from './material/development-cards/DevelopmentCard'
 import Empire from './material/Empire'
 import EmpireCard from './material/empire-cards/EmpireCard'
 import Move from './moves/Move'
+import artwork from './material/its-cover-artwork.png'
 
 const topLeft = css`
   position: absolute;
@@ -26,13 +27,22 @@ export default function () {
   if (!game)
     return null
   return (
-    <Game>
+    <Game style={{backgroundImage: `url(${artwork})`, backgroundSize: 'cover'}}>
       <Header/>
       <p>{JSON.stringify(game)}</p>
       {Object.entries(game.players).map(([empire, player]) => (
         <Fragment key={empire}>
           <EmpireCard empire={empire as Empire} position={empire === Empire.PanafricanUnion ? bottomRight : topLeft}/>
-          {player.hand.map((development, index) => <DevelopmentCard key={index} development={development}/>)}
+          <Hand rotationOrigin={5000} nearbyMaxRotation={0.72} sizeRatio={65/100} position={empire === Empire.PanafricanUnion ? css`
+            bottom: 3vh;
+            left: calc(50% - 20vh);
+          ` : css`
+            transform: rotate(180deg) scale(0.5);
+            top: 10vh;
+            left: calc(50% - 10vh);
+          `}>
+            {player.hand.map((development, index) => <DevelopmentCard key={index} development={development}/>)}
+          </Hand>
         </Fragment>
       ))}
     </Game>
