657aa859246fcb8ca932a5c23f4e766e6d108232
Romain Fromi <romain.fromi@gmail.com>
2020-02-13 11:29:19 +0100

    Fix npm, webpack and typescript build configuration to avoid duplicate library bugs on the website

--- webpack.build.js
+++ webpack.build.js
@@ -12,7 +12,11 @@ module.exports = merge(commonConfig, {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    libraryTarget: 'commonjs2'
+    library: 'ItsAWonderfulWorld',
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true,
+    globalObject: 'this'
   },
   plugins: [new CleanWebpackPlugin()],
   module: {
@@ -27,5 +31,28 @@ module.exports = merge(commonConfig, {
         }
       }
     ]
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+      'tabletop-game-workshop': path.resolve(__dirname, './node_modules/tabletop-game-workshop')
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM'
+    },
+    'tabletop-game-workshop': 'tabletop-game-workshop'
   }
 })
