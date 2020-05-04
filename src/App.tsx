7846fb6c57b2c5b7386738a54f57f85693d59356
Romain Fromi <romain.fromi@gmail.com>
2020-05-04 22:46:30 +0200

    Add i18next initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import {css, Global} from '@emotion/core'
 import {useGame} from '@interlude-games/workshop'
 import normalize from 'emotion-normalize'
+import i18next from 'i18next'
+import ICU from 'i18next-icu'
 import React, {FunctionComponent} from 'react'
 import {DndProvider} from 'react-dnd'
 import MultiBackend from 'react-dnd-multi-backend'
 import HTML5ToTouch from 'react-dnd-multi-backend/dist/cjs/HTML5toTouch'
+import {initReactI18next} from 'react-i18next'
 import Game from './Game'
 import Header from './Header'
 import {ItsAWonderfulWorldView} from './ItsAWonderfulWorld'
 import artwork from './material/its-cover-artwork.png'
+
+i18next.use(initReactI18next).use(ICU)
+
+i18next.init({
+  lng: 'fr',
+  fallbackLng: 'en',
+  keySeparator: '>',
+  nsSeparator: '|',
+  resources: {
+    en: {
+      website: {}
+    },
+    fr: {
+      website: {}
+    }
+  }
+})
 
 const App: FunctionComponent = () => {
   const game = useGame<ItsAWonderfulWorldView>()
