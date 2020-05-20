1195214a0a782cf6c4567b7096532683f39e981f
JulienOrigames <65389596+JulienOrigames@users.noreply.github.com>
2020-05-20 18:32:01 +0200

    Add buttons on zoomed draft cards

--- src/players/DraftArea.tsx
+++ src/players/DraftArea.tsx
@@ -1,6 +1,6 @@
 import {css} from '@emotion/core'
 import {Draggable, useAnimation, usePlay, usePlayerId} from '@interlude-games/workshop'
-import React, {FunctionComponent, useState} from 'react'
+import React, {FunctionComponent, useEffect, useState} from 'react'
 import {useDrop} from 'react-dnd'
 import {developmentFromDraftArea} from '../drag-objects/DevelopmentFromDraftArea'
 import DevelopmentFromHand from '../drag-objects/DevelopmentFromHand'
@@ -16,11 +16,16 @@ import Phase from '../types/Phase'
 import Player from '../types/Player'
 import PlayerView from '../types/PlayerView'
 import screenRatio from '../util/screenRatio'
-import {popupBackgroundStyle} from '../util/Styles'
 import {constructedCardLeftMargin} from './ConstructedCardsArea'
 import {bottomMargin} from './DisplayedEmpire'
+import {popupBackgroundStyle} from '../util/Styles'
+import {isPlayer} from '../types/typeguards'
+import {slateForConstruction} from '../moves/SlateForConstruction'
+import {recycle} from '../moves/Recycle'
+import {useTranslation} from 'react-i18next'
 
 const DraftArea: FunctionComponent<{ game: GameView, player: Player | PlayerView }> = ({game, player}) => {
+  const {t} = useTranslation()
   const row = game.phase === Phase.Draft ? 1 : 0
   const playerId = usePlayerId<EmpireName>()
   const play = usePlay()
@@ -36,11 +41,30 @@ const DraftArea: FunctionComponent<{ game: GameView, player: Player | PlayerView
     }),
     drop: (item: DevelopmentFromHand) => play(chooseDevelopmentCard(player.empire, item.card))
   })
+  useEffect(() => {
+    if ( focusedCard !== player.chosenCard && !player.draftArea.some(card => card === focusedCard)) {
+      setFocusedCard(undefined)
+    }
+  }, [player, focusedCard])
   return (
     <>
-      {focusedCard !== undefined && <div css={popupBackgroundStyle} onClick={() => setFocusedCard(undefined)}/>}
+      {focusedCard !== undefined &&
+      <>
+          <div css={popupBackgroundStyle} onClick={() => setFocusedCard(undefined)}/>
+        {isPlayer(player) && game.phase === Phase.Planning &&
+        <>
+            <button css={draftConstructionButton} onClick={() => play(slateForConstruction(player.empire,focusedCard))}>
+              {t('Construire')}
+            </button>
+            <button css={draftRecyclingButton} onClick={() => play(recycle(player.empire,focusedCard))}>
+              {t('Recycler')}
+            </button>
+        </>
+        }
+      </>
+      }
       <div ref={ref} css={getDraftAreaStyle(row, game.players.length === 2, isValidTarget, isOver)}>
-        {!player.draftArea.length && <span css={draftAreaText}>Zone de draft</span>}
+        {!player.draftArea.length && <span css={draftAreaText}>{t('Zone de draft')}</span>}
       </div>
       {player.draftArea.map((card, index) => (
         <Draggable key={card} item={developmentFromDraftArea(card)} css={getAreaCardStyle(row, index, focusedCard === card)}
@@ -50,7 +74,8 @@ const DraftArea: FunctionComponent<{ game: GameView, player: Player | PlayerView
         </Draggable>
       ))}
       {chosenCard && <DevelopmentCard development={chosenCard !== true ? developmentCards[chosenCard] : undefined}
-                                      css={[getAreaCardStyle(row, player.draftArea.length), choosingDevelopment && css`opacity: 0;`]}/>}
+                                      css={[getAreaCardStyle(row, player.draftArea.length,focusedCard === chosenCard), choosingDevelopment && css`opacity: 0;`]}
+                                      onClick={() => typeof chosenCard == 'number' && setFocusedCard(chosenCard)} />}
     </>
   )
 }
@@ -131,5 +156,49 @@ const draftAreaText = css`
   color: darkgreen;
   font-size: 4vh;
 `
+const draftConstructionButton = css`
+  position: absolute;
+  z-index: 100;
+  bottom: 51%;
+  right: ${51 + cardWidth * 1.5}%;
+  box-shadow: inset 0 0.1vh 0 0 #ffffff;
+  background: #ededed linear-gradient(to bottom, #ededed 5%, #dfdfdf 100%);
+  border-radius: 2vh;
+  border: 0.1vh solid #dcdcdc;
+  color: #333333;
+  padding: 0 2vh 0 1vh;
+  font-size: 4.8vh;
+  filter: drop-shadow(0.1vh 0.1vh 0.5vh black);
+  &:hover, &:focus {
+    outline:0;
+    background: #dfdfdf linear-gradient(to bottom, #dfdfdf 5%, #ededed 100%);
+  }
+  &:active {
+    transform: translateY(1px);
+  }
+`
+
+const draftRecyclingButton = css`
+  position: absolute;
+  z-index: 100;
+  top: 51%;
+  right: ${51 + cardWidth * 1.5}%;
+  box-shadow: inset 0 0.1vh 0 0 #ffffff;
+  background: #ededed linear-gradient(to bottom, #ededed 5%, #dfdfdf 100%);
+  border-radius: 2vh;
+  border: 0.1vh solid #dcdcdc;
+  color: #333333;
+  padding: 0 2vh 0 1vh;
+  font-size: 4.8vh;
+  filter: drop-shadow(0.1vh 0.1vh 0.5vh black);
+  &:hover, &:focus {
+    outline:0;
+    background: #dfdfdf linear-gradient(to bottom, #dfdfdf 5%, #ededed 100%);
+  }
+  &:active {
+    transform: translateY(1px);
+  }
+`
+
 
 export default DraftArea
