e8fe6d83e1090f4bb710a0601733a18b4d867723
Romain Fromi <romain.fromi@gmail.com>
2020-05-28 18:59:02 +0200

    Fix loading spinner size for mobile devices

--- src/util/LoadingSpinner.tsx
+++ src/util/LoadingSpinner.tsx
@@ -22,23 +22,23 @@ const keyframe = keyframes`
 const style = css`
   display: inline-block;
   position: relative;
-  width: 80px;
-  height: 80px;
+  width: 8vh;
+  height: 8vh;
   & > div {
     animation-name: ${keyframe};
     animation-duration: 1.2s;
     animation-timing-function: cubic-bezier(0.5, 0, 0.5, 1);
     animation-iteration-count: infinite;
-    transform-origin: 40px 40px;
+    transform-origin: 4vh 4vh;
     &:after {
       content: " ";
       display: block;
       position: absolute;
-      width: 7px;
-      height: 7px;
+      width: 0.7vh;
+      height: 0.7vh;
       border-radius: 50%;
       background: #333;
-      margin: -4px 0 0 -4px;
+      margin: -0.4vh 0 0 -0.4vh;
     }
   }
 `
@@ -46,64 +46,64 @@ const style = css`
 const bullet1 = css`
   animation-delay: -0.036s;
   &:after {
-    top: 63px;
-    left: 63px;
+    top: 6.3vh;
+    left: 6.3vh;
   }
 `
 
 const bullet2 = css`
   animation-delay: -0.072s;
   &:after {
-    top: 68px;
-    left: 56px;
+    top: 6.8vh;
+    left: 5.6vh;
   }
 `
 
 const bullet3 = css`
   animation-delay: -0.108s;
   &:after {
-    top: 71px;
-    left: 48px;
+    top: 7.1vh;
+    left: 4.8vh;
   }
 `
 
 const bullet4 = css`
   animation-delay: -0.144s;
   &:after {
-    top: 72px;
-    left: 40px;
+    top: 7.2vh;
+    left: 4vh;
   }
 `
 
 const bullet5 = css`
   animation-delay: -0.18s;
   &:after {
-    top: 71px;
-    left: 32px;
+    top: 7.1vh;
+    left: 3.2vh;
   }
 `
 
 const bullet6 = css`
   animation-delay: -0.216s;
   &:after {
-    top: 68px;
-    left: 24px;
+    top: 6.8vh;
+    left: 2.4vh;
   }
 `
 
 const bullet7 = css`
   animation-delay: -0.252s;
   &:after {
-    top: 63px;
-    left: 17px;
+    top: 6.3vh;
+    left: 1.7vh;
   }
 `
 
 const bullet8 = css`
   animation-delay: -0.288s;
   &:after {
-    top: 56px;
-    left: 12px;
+    top: 5.6vh;
+    left: 1.2vh;
   }
 `
 
