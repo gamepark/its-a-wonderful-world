1ca100983eb913e52006897b3f009c62900ca33e
Romain Fromi <romain.fromi@gmail.com>
2020-06-03 19:05:55 +0200

    Improve Round Tracker position and size

--- src/material/board/RoundTracker.tsx
+++ src/material/board/RoundTracker.tsx
@@ -1,34 +1,35 @@
+import {css} from '@emotion/core'
+import React, {FunctionComponent} from 'react'
+import {useTranslation} from 'react-i18next'
+import {constructedCardX, headerHeight} from '../../util/Styles'
 import roundTracker1 from './round-tracker-1-3.png'
 import roundTracker2 from './round-tracker-2-4.png'
-import {FunctionComponent} from 'react'
-import {useTranslation} from 'react-i18next'
-import React from 'react'
-import {css} from '@emotion/core'
 
 
 const RoundTracker: FunctionComponent<{ round: number }> = ({round}) => {
   const {t} = useTranslation()
   return (
     <>
-      <img alt={t('Le marqueur de tour')} src={round % 2 ? roundTracker1 : roundTracker2} draggable="false"
-             css={roundTrackerStyle}/>
-             <span css={roundTextStyle}>{round}</span>
+      <img alt={t('Le marqueur de tour')} src={round % 2 ? roundTracker1 : roundTracker2} draggable="false" css={roundTrackerStyle}/>
+      <span css={roundTextStyle}>{round}</span>
     </>
   )
 }
 
 const roundTrackerStyle = css`
   position: absolute;
-  height: 12%;
-  top: 7%;
-  left: 0%;
+  z-index: 1;
+  height: 10.5%;
+  top: ${headerHeight + 0.5}%;
+  left: ${constructedCardX}%;
   transition: left 0.5s ease-in-out, transform 0.5s ease-in-out;
 `
 
 const roundTextStyle = css`
   position: absolute;
-  top: 10.5%;
-  left: 3%;
+  z-index: 2;
+  top: 10.3%;
+  left: 4.3%;
   font-size: 4vh;
   color: #333;
   font-weight: bold;
