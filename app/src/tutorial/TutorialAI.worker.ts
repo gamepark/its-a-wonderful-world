f502c027e1649bb03d4e5005aff9ae62afe22fa0
Romain Fromi <romain.fromi@gmail.com>
2021-10-10 11:23:42 +0200

    When service worker are not available, there might be a "too much recursion" error, on Firefox for example. Prevent such error from blocking the tutorial by using a fallback on the Dummy AI.

--- app/src/tutorial/TutorialAI.worker.ts
+++ app/src/tutorial/TutorialAI.worker.ts
@@ -1,8 +1,15 @@
 import GameState from '@gamepark/its-a-wonderful-world/GameState'
-import GameView from '@gamepark/its-a-wonderful-world/GameView'
+import ItsAWonderfulWorld from '@gamepark/its-a-wonderful-world/ItsAWonderfulWorld'
 import EmpireName from '@gamepark/its-a-wonderful-world/material/EmpireName'
+import {Dummy} from '@gamepark/rules-api'
 import TutorialAI from './TutorialAI'
 
-export async function ai(game: GameView | GameState, playerId: EmpireName) {
-  return new TutorialAI(playerId).play(game as GameState)
+export async function ai(game: GameState, playerId: EmpireName) {
+  try {
+    return new TutorialAI(playerId).play(game as GameState)
+  } catch (e) {
+    console.error(e)
+    const dummy = new Dummy(ItsAWonderfulWorld)
+    return dummy.getRandomMove(game, playerId)
+  }
 }
